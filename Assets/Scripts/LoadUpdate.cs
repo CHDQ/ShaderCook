189e44c9ee2748c828750b48b4aa663dd08be89f
qiong.duan <qiong.duan@langtaojin.com>
2023-12-04 17:49:31 +0800

    shader study

---
+++ Assets/Scripts/LoadUpdate.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadUpdate : MonoBehaviour
+{
+    public Material myMaterial;
+
+    public int load;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myMaterial.shader.maximumLOD = load;
+    }
+}
